7885d10c2a7db0de1aa688b194ff14da4e8748ba
Anawatratt <110977191+Anawatratt@users.noreply.github.com>
2023-08-30 13:05:26 +0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,24 +3,22 @@ import { Button,Container,Text,Textarea,Title,Divider,Group,Pagination,Rating} f
 
 export default function FoodReviewPage() {
   return (
-
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
 
-      <Title order={4} mt='sm'>
+      <Title order={4} mt={"sm"}>
         Your rating
       </Title>
+      <Rating size="lg" />
 
-      <Rating size="lg"/>
-
-      <Textarea 
-      label="Your review" 
-      placeholder="Do you enjoy eating?"
-      mt="xs"
-      minRows={3}
+      <Textarea
+        placeholder="Do you enjoy eating?"
+        label="Your review"
+        mt={"xs"}
+        minRows={3}
       />
 
-      <Button mt="xs" color="orange">
+      <Button mt={"xs"} color="orange">
         Submit Review
       </Button>
 
